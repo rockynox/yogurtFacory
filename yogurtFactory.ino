6e6943ca289fcdef1b93d0bdf0081a4349bab22d
Léo <leo.maisonneuve-extern@renault.com>
2019-04-03 09:12:28 +0200

    Time keeper added

--- yogurtFactory.ino
+++ yogurtFactory.ino
@@ -1,29 +1,26 @@
-#include <Wire.h> // must be included here so that Arduino library object file references work
+#include <Wire.h>
 #include <RtcDS3231.h>
 
 #include <HardwareSerial.h>
+
 int RELAY = D3;
-int COOKING_BUTTON = D1;
-int STOP_COOKING_BUTTON = D2;
-const byte COOKING_LED_RED = D5;
-const byte COOKING_LED_GREEN = D6;
+int COOKING_LED_RED = D5;
+int COOKING_LED_GREEN = D6;
+int COOKING_LED_BLUE = D7;
+int COOKING_BUTTON = D8;
 
-
-const byte COOKING_LED_BLUE = D7;
 int relayInitPosition = LOW;
-
 int relayCookingPosition = HIGH;
 
-int currentCookingLedColor[3] = {0, 0, 0};
-int initialCookingTimeInMilliseconds = 10 * 60 * 1000;
+//int currentCookingLedColor[3] = {0, 0, 0};
+int cookingTimeInSeconds = 1 * 60;
 bool cookingInProgress;
-
-int remainingCookingTimeInMilliseconds;
+RtcDateTime startCookingDate;
 
 void setup() {
+    Serial.begin(57600);
 
     setupTimeKeeper();
-    Serial.begin(57600);
 
     pinMode(RELAY, OUTPUT);
     pinMode(COOKING_LED_RED, OUTPUT);
@@ -33,44 +30,47 @@ void setup() {
 
     digitalWrite(RELAY, relayInitPosition);
     cookingInProgress = false;
-    remainingCookingTimeInMilliseconds = 0;
 
-    setCookingLedColor(0, 0, 0);
+    blinkHello();
 }
 
 void loop() {
+    RtcDateTime now = getTime();
     if (cookingInProgress) {
-        keepCooking();
-        bool stopCookingButtonPressed = digitalRead(STOP_COOKING_BUTTON);
-        if (remainingCookingTimeInMilliseconds < 0 || stopCookingButtonPressed) {
+        printDate(now);
+        bool cookingButtonPressed = digitalRead(COOKING_BUTTON);
+        if (isCookingOver(now) || cookingButtonPressed) {
             stopCoooking();
         }
     }
     else {
         bool cookingButtonPressed = digitalRead(COOKING_BUTTON);
         if (cookingButtonPressed) {
-            startCooking();
+            startCooking(now);
+            waitInMillisecond(600);
         }
     }
     setLedIndicator();
 }
 
-void startCooking() {
+void startCooking(RtcDateTime now) {
     cookingInProgress = true;
-    remainingCookingTimeInMilliseconds = initialCookingTimeInMilliseconds;
+    startCookingDate = now;
     digitalWrite(RELAY, relayCookingPosition);
     Serial.println("Started Cooking !");
 }
 
-void keepCooking() {
-    waitInMillisecond(1);
-    remainingCookingTimeInMilliseconds--;
-}
 void stopCoooking() {
     cookingInProgress = false;
     digitalWrite(RELAY, relayInitPosition);
     blinkCookingLedInRed();
     Serial.println("Stopped Cooking !");
+}
+
+bool isCookingOver(RtcDateTime now) {
+    int nowInSeconds = getUnixTime(now);
+    int startCookingDateInSeconds = getUnixTime(startCookingDate);
+    return (nowInSeconds - startCookingDateInSeconds) > cookingTimeInSeconds;
 }
 
 void setLedIndicator() {
@@ -91,6 +91,27 @@ void blinkCookingLedInRed() {
     }
 }
 
+void blinkHello() {
+    int blinkSpeed = 200;
+    setCookingLedColor(255, 38, 0);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(255, 147, 0);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(255, 251, 0);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(142, 250, 0);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(0, 250, 146);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(0, 253, 255);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(4, 51, 255);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(255, 64, 255);
+    waitInMillisecond(blinkSpeed);
+    setCookingLedColor(0, 0, 0);
+}
+
 void setCookingLedColor(unsigned int red, unsigned int green, unsigned int blue) {
     analogWrite(COOKING_LED_RED, red);
     analogWrite(COOKING_LED_GREEN, green);
@@ -101,27 +122,19 @@ void waitInMillisecond(int millisecondToWait) {
     delay(millisecondToWait);
 }
 
-
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 RtcDS3231 <TwoWire> Rtc(Wire);
-
-/* for normal hardware wire use above */
 
 void setupTimeKeeper() {
     Serial.print("compiled: ");
     Serial.print(__DATE__);
     Serial.println(__TIME__);
 
-    //--------RTC SETUP ------------
-    // if you are using ESP-01 then uncomment the line below to reset the pins to
-    // the available pins for SDA, SCL
-    // Wire.begin(0, 2); // due to limited pins, use pin 0 and 2 for SDA, SCL
-
+    Wire.begin(D1, D2); //(SDA, SCL)
     Rtc.Begin();
 
     RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
     printDate(compiled);
-    Serial.println();
 
     if (!Rtc.IsDateTimeValid()) {
         if (Rtc.LastError() != 0) {
@@ -163,8 +176,6 @@ void setupTimeKeeper() {
         Serial.println("RTC is the same as compile time! (not expected but all is fine)");
     }
 
-    // never assume the Rtc was last configured by you, so
-    // just clear them to your needed state
     Rtc.Enable32kHzPin(false);
     Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
 }
@@ -184,10 +195,7 @@ RtcDateTime getTime() {
             Serial.println("RTC lost confidence in the DateTime!");
         }
     }
-
     RtcDateTime now = Rtc.GetDateTime();
-    printDate(now);
-
     return now;
 }
 
@@ -197,5 +205,10 @@ void printDate(const RtcDateTime &dt) {
     snprintf_P(datestring, countof(datestring), PSTR("%02u/%02u/%04u %02u:%02u:%02u"), dt.Month(), dt.Day(), dt.Year(),
                dt.Hour(), dt.Minute(), dt.Second());
     Serial.println(datestring);
+}
+
+int getUnixTime(RtcDateTime date) {
+    return ((((((date.Year() - 1970)) * 12 + date.Month()) * 30 + date.Day()) * 24 + date.Hour()) * 60 +
+            date.Minute()) * 60 + date.Second();
 }
 
